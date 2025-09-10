52c72fc56590d4aff9c8e15d27194a298cd4a364
Aldair Barros <aldairbarros@outlook.com>
2025-09-10 13:51:29 -0400

    \remover testes do build docker e ignorar /tests localmente\

--- tests/files/criar_docx.py
+++
@@ -1,7 +0,0 @@
-from docx import Document
-
-doc = Document()
-doc.add_heading('A Trindade na Teologia Cristã', 0)
-doc.add_paragraph('A doutrina da Trindade afirma que Deus é um só, em três pessoas: Pai, Filho e Espírito Santo.')
-doc.add_paragraph('Esta verdade é fundamental para a fé cristã e está presente em todo o Novo Testamento.')
-doc.save('j:/Projetos Python/eklesia.ia.python/tests/files/teste.docx')
